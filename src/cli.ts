1ade1dcb1e491b1ac036b4c6b922ce92b2f4d1a9
Jeffrey Paul <sneak@sneak.berlin>
2018-02-05 15:34:56 -0600

    remove semicolons and use single quotes for mr particular

--- src/cli.ts
+++ src/cli.ts
@@ -1,149 +1,149 @@
-import * as program from "commander";
-import { inspect } from "util";
-import { VError } from "verror";
-import { RPC, RPCSigner } from "./index";
+import * as program from 'commander'
+import { inspect } from 'util'
+import { VError } from 'verror'
+import { RPC, RPCSigner } from './index'
 
-const { argv, env, exit, stderr, stdin, stdout } = process;
+const { argv, env, exit, stderr, stdin, stdout } = process
 
-const KEYWORD_PATTERN = /^[0-9a-z]+:?=.+$/i;
+const KEYWORD_PATTERN = /^[0-9a-z]+:?=.+$/i
 
 function log(out: NodeJS.WriteStream, ...msgs) {
     const fmt = out.isTTY
         ? v => inspect(v, { colors: true, depth: null })
-        : JSON.stringify;
+        : JSON.stringify
     for (const msg of msgs) {
-        out.write(typeof msg === "string" ? msg : fmt(msg));
+        out.write(typeof msg === 'string' ? msg : fmt(msg))
     }
 }
 
 function parseKeywordParam(param: string) {
-    let idx = param.indexOf("=");
+    let idx = param.indexOf('=')
     if (idx === -1) {
-        throw new VError({ name: "InvalidParam" }, "Missing key=value pair");
+        throw new VError({ name: 'InvalidParam' }, 'Missing key=value pair')
     }
-    let rawValue = param.slice(idx + 1);
-    if (param[idx - 1] === ":") {
-        rawValue = ":" + rawValue;
-        idx -= 1;
+    let rawValue = param.slice(idx + 1)
+    if (param[idx - 1] === ':') {
+        rawValue = ':' + rawValue
+        idx -= 1
     }
-    const key = param.slice(0, idx);
-    let value: any;
+    const key = param.slice(0, idx)
+    let value: any
     try {
-        value = parseParam(rawValue);
+        value = parseParam(rawValue)
     } catch (cause) {
         throw new VError(
-            { name: "InvalidParam", cause },
+            { name: 'InvalidParam', cause },
             `Unable to parse '${key}'`
-        );
+        )
     }
-    return { key, value };
+    return { key, value }
 }
 
 function parseParam(param: string) {
-    if (param[0] === ":") {
-        return JSON.parse(param.slice(1));
+    if (param[0] === ':') {
+        return JSON.parse(param.slice(1))
     }
-    return param;
+    return param
 }
 
 function readStream(stream: NodeJS.ReadStream) {
     return new Promise<Buffer>((resolve, reject) => {
-        const chunks: Buffer[] = [];
-        stream.on("data", chunk => {
-            chunks.push(chunk);
-        });
-        stream.on("end", () => {
-            resolve(Buffer.concat(chunks));
-        });
-        stream.on("error", reject);
-    });
+        const chunks: Buffer[] = []
+        stream.on('data', chunk => {
+            chunks.push(chunk)
+        })
+        stream.on('end', () => {
+            resolve(Buffer.concat(chunks))
+        })
+        stream.on('error', reject)
+    })
 }
 
 interface Options {
-    address: string;
-    raw: boolean;
-    sign: boolean;
-    verbose: boolean;
+    address: string
+    raw: boolean
+    sign: boolean
+    verbose: boolean
 }
 
 async function main(method: string, params: string[], options: Options) {
     if (options.verbose) {
-        log(stderr, { method, params, options }, "\n");
+        log(stderr, { method, params, options }, '\n')
     }
-    let parsedParams: any;
-    if (params.length === 1 && ["-", "_"].includes(params[0])) {
-        const input = await readStream(stdin);
+    let parsedParams: any
+    if (params.length === 1 && ['-', '_'].includes(params[0])) {
+        const input = await readStream(stdin)
         try {
-            if (params[0] === "_") {
+            if (params[0] === '_') {
                 parsedParams = input
-                    .toString("utf8")
-                    .split("\n")
+                    .toString('utf8')
+                    .split('\n')
                     .map(line => line.trim())
                     .filter(line => line.length)
-                    .map(line => JSON.parse(line));
+                    .map(line => JSON.parse(line))
             } else {
-                parsedParams = JSON.parse(input.toString("utf8"));
+                parsedParams = JSON.parse(input.toString('utf8'))
             }
         } catch (cause) {
             throw new VError(
-                { name: "InvalidParam", cause },
-                "Unable to parse stdin"
-            );
+                { name: 'InvalidParam', cause },
+                'Unable to parse stdin'
+            )
         }
     } else {
         if (params.some(param => KEYWORD_PATTERN.test(param))) {
-            parsedParams = {};
+            parsedParams = {}
             for (const { key, value } of params.map(parseKeywordParam)) {
-                parsedParams[key] = value;
+                parsedParams[key] = value
             }
         } else {
-            parsedParams = [];
+            parsedParams = []
             for (let i = 0; i < params.length; i++) {
                 try {
-                    parsedParams.push(parseParam(params[i]));
+                    parsedParams.push(parseParam(params[i]))
                 } catch (cause) {
                     throw new VError(
-                        { name: "InvalidParam", cause },
+                        { name: 'InvalidParam', cause },
                         `Unable to parse param at index ${i}`
-                    );
+                    )
                 }
             }
         }
     }
-    const client = new RPC(options.address);
-    let request = client.buildRequest(method, parsedParams);
+    const client = new RPC(options.address)
+    let request = client.buildRequest(method, parsedParams)
     if (options.verbose) {
-        log(stderr, request, "\n");
+        log(stderr, request, '\n')
     }
     if (options.sign) {
         const signer: RPCSigner = {
             account: env.RPCIT_ACCOUNT as string,
             key: env.RPCIT_KEY as string
-        };
+        }
         if (!signer.account || !signer.key) {
             throw new VError(
-                { name: "SigningError" },
-                "Credentials missing, make sure to set RPCIT_ACCOUNT and RPCIT_KEY"
-            );
+                { name: 'SigningError' },
+                'Credentials missing, make sure to set RPCIT_ACCOUNT and RPCIT_KEY'
+            )
         }
         try {
-            request = client.signRequest(signer, request);
+            request = client.signRequest(signer, request)
         } catch (cause) {
             throw new VError(
-                { name: "SigningError", cause },
-                "Unable to sign request"
-            );
+                { name: 'SigningError', cause },
+                'Unable to sign request'
+            )
         }
         if (options.verbose) {
-            log(stderr, request, "\n");
+            log(stderr, request, '\n')
         }
     }
-    const response = await client.send(request);
-    const out = options.raw ? response : response.error || response.result;
+    const response = await client.send(request)
+    const out = options.raw ? response : response.error || response.result
     if (out != null) {
-        log(stdout, out, "\n");
+        log(stdout, out, '\n')
     }
-    return !options.raw && response.error ? 1 : 0;
+    return !options.raw && response.error ? 1 : 0
 }
 
 /*______________"\
@@ -155,7 +155,7 @@ async function main(method: string, params: string[], options: Options) {
  .---------------.__|||              |||
 ( . ACTIONsaw__  |__||=:____________:=||
  '---------------'   \VVVVVVVVVVVVVVV*/
-let sawAction = false;
+let sawAction = false
 
 const extraHelp = `
 
@@ -191,49 +191,49 @@ const extraHelp = `
     Get user data from conveyor
       $ RPCIT_ACCOUNT=admin RPCIT_KEY=5POSTINGWIF rpcit -s conveyor.get_user_data user
 
-`;
+`
 
 program
-    .arguments("<method> [params...]")
+    .arguments('<method> [params...]')
     .option(
-        "-a, --address [url]",
-        "address to the RPC server",
-        "https://api.steemit.com"
+        '-a, --address [url]',
+        'address to the RPC server',
+        'https://api.steemit.com'
     )
-    .option("--dev", "set address to https://api.steemitdev.com")
-    .option("--stage", "set address to https://api.steemitstage.com")
+    .option('--dev', 'set address to https://api.steemitdev.com')
+    .option('--stage', 'set address to https://api.steemitstage.com')
     .option(
-        "-s, --sign",
-        "sign the request using credentials from env vars RPCIT_ACCOUNT and RPCIT_KEY"
+        '-s, --sign',
+        'sign the request using credentials from env vars RPCIT_ACCOUNT and RPCIT_KEY'
     )
-    .option("-r, --raw", "write the raw JSON-RPC 2.0 response to stdout")
-    .option("-v, --verbose", "print debug info to stderr")
+    .option('-r, --raw', 'write the raw JSON-RPC 2.0 response to stdout')
+    .option('-v, --verbose', 'print debug info to stderr')
     .action((method, params) => {
-        sawAction = true;
-        const { sign, verbose, raw } = program;
-        let address = program.address;
+        sawAction = true
+        const { sign, verbose, raw } = program
+        let address = program.address
         if (program.stage) {
-            address = "https://api.steemitstage.com";
+            address = 'https://api.steemitstage.com'
         }
         if (program.dev) {
-            address = "https://api.steemitdev.com";
+            address = 'https://api.steemitdev.com'
         }
         main(method, params, { sign, verbose, address, raw })
             .then(code => {
-                process.exitCode = code;
+                process.exitCode = code
             })
             .catch(error => {
-                stderr.write(String(error));
-                stderr.write("\n");
-                exit(1);
-            });
+                stderr.write(String(error))
+                stderr.write('\n')
+                exit(1)
+            })
     })
-    .on("--help", () => {
-        log(stdout, extraHelp);
+    .on('--help', () => {
+        log(stdout, extraHelp)
     })
-    .parse(argv);
+    .parse(argv)
 
 if (!sawAction) {
-    stderr.write("No method specified. For usage see --help\n");
-    exit(1);
+    stderr.write('No method specified. For usage see --help\n')
+    exit(1)
 }
